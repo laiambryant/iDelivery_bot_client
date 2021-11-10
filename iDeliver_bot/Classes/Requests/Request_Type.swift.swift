9c355983a49857fa1dcdcfee7263cab7df2b221a
Liam Bryant <liambryant@outlook.it>
2021-11-10 18:20:07 +0100

    Finished basic implementation of client c++ library

---
+++ iDeliver_bot/Classes/Requests/Request_Type.swift.swift
@@ -0,0 +1,8 @@
+//
+//  Request_Type.swift.swift
+//  iDeliver_bot
+//
+//  Created by Liam Bryant on 09/11/21.
+//
+
+import Foundation
---
+++ iDeliver_bot/Classes/sockaddr.swift
@@ -0,0 +1,8 @@
+//
+//  sockaddr.swift
+//  iDeliver_bot
+//
+//  Created by Liam Bryant on 10/11/21.
+//
+
+import Foundation
